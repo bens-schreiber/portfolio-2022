2cedf3b5c593f71968c38593ee9acc9fe54e439a
Ben Schreiber <bpschreiber2003@gmail.com>
2022-04-20 12:35:22 -0700

    added expandable button for projects

--- src/pages/portfolio/components/software-engineer-projects/software-engineer-projects.tsx
+++ src/pages/portfolio/components/software-engineer-projects/software-engineer-projects.tsx
@@ -4,7 +4,6 @@ import { FblaSVG } from "../../../home/components/about/assets";
 export default function SoftwareEngineerProjects() {
     return <>
         <div className="sft-engin">
-
             <div className="headers">
                 <h1>
                     SOFTWARE ENGINEERING
@@ -16,63 +15,77 @@ export default function SoftwareEngineerProjects() {
                     <img src={EzQuizLogo} />
                 </div>
             </div>
+            
+            <button onClick={expandClicked} className="expand-button">CLICK TO EXPAND!</button>
 
-            <div className="sft-engin-about">
-                <div className="about sft-grid-box">
-                    <h1 className="bold-header">ABOUT</h1>
-                    <p>EzQuiz was designed for competition in the <span>2021 FBLA Coding & Programming</span> category. EzQuiz creates and administers fully customizeable, secure, locally hosted quizzes.</p>
+            <input id="collapse" type="checkbox"/>
+            <div className="collapse">
+
+                <div className="sft-engin-about">
+                    <div className="about sft-grid-box">
+                        <h1 className="bold-header">ABOUT</h1>
+                        <p>EzQuiz was designed for competition in the <span>2021 FBLA Coding & Programming</span> category. EzQuiz creates and administers fully customizeable, secure, locally hosted quizzes.</p>
+                    </div>
+                    <div className="awards sft-grid-box">
+                        <img src={FblaSVG} />
+                        <h1>FBLA 3rd in Nation</h1>
+                        <h1>FBLA 1st in Washington State</h1>
+                    </div>
+                    <div className="tech sft-grid-box">
+                        <img src={JavaLogo} />
+                        <p>Developed primarily in Java with JavaFX</p>
+                    </div>
+                    <div className="guidelines sft-grid-box">
+                        <h1 className="bold-header">GUIDELINES</h1>
+                        <p>The project prompt, in summary (see <a href="https://www.fbla-pbl.org/media/2021/08/FBLA-Rating-Sheets.pdf">Judge Scoring Guidelines</a> for the complete), was required to have 50 questions in a relational database fetched by the software, present the user with a quiz, and inform them of their score.</p>
+                    </div>
                 </div>
-                <div className="awards sft-grid-box">
-                    <img src={FblaSVG} />
-                    <h1>FBLA 3rd in Nation</h1>
-                    <h1>FBLA 1st in Washington State</h1>
+
+                <div className="front-end">
+                    <div className="headers">
+                        <h1>
+                            FRONT END
+                        </h1>
+                    </div>
+                    <div className="demo-vids">
+                        <video className="sft-grid-box" controls >
+                            <source src={Demo1} />
+                            Sorry, your browser doesn't support embedded videos.
+                        </video>
+                        <video className="sft-grid-box" controls>
+                            <source src={Demo2} />
+                            Sorry, your browser doesn't support embedded videos.
+                        </video>
+                    </div>
+                    <div className="headers">
+                        <h1>
+                            USER FLOW
+                        </h1>
+                    </div>
+                    <div className="user-flow-diagram">
+                        <img className="sft-grid-box" src={UserFlow} />
+                    </div>
                 </div>
-                <div className="tech sft-grid-box">
-                    <img src={JavaLogo} />
-                    <p>Developed primarily in Java with JavaFX</p>
-                </div>
-                <div className="guidelines sft-grid-box">
-                    <h1 className="bold-header">GUIDELINES</h1>
-                    <p>The project prompt, in summary (see <a href="https://www.fbla-pbl.org/media/2021/08/FBLA-Rating-Sheets.pdf">Judge Scoring Guidelines</a> for the complete), was required to have 50 questions in a relational database fetched by the software, present the user with a quiz, and inform them of their score.</p>
+
+                <div className="back-end">
+                    <div className="headers">
+                        <h1>
+                            BACK END
+                        </h1>
+                    </div>
+                    <div className="uml-diagram">
+                        <img className="sft-grid-box" src={UMLDiagramEzQuiz} />
+                    </div>
                 </div>
             </div>
 
-            <div className="front-end">
-                <div className="headers">
-                    <h1>
-                        FRONT END
-                    </h1>
-                </div>
-                <div className="demo-vids">
-                    <video className="sft-grid-box" controls >
-                        <source src={Demo1} />
-                        Sorry, your browser doesn't support embedded videos.
-                    </video>
-                    <video className="sft-grid-box" controls>
-                        <source src={Demo2} />
-                        Sorry, your browser doesn't support embedded videos.
-                    </video>
-                </div>
-                <div className="headers">
-                    <h1>
-                        USER FLOW
-                    </h1>
-                </div>
-                <div className="user-flow-diagram">
-                    <img className="sft-grid-box" src={UserFlow} />
-                </div>
-            </div>
-
-            <div className="back-end">
-                <div className="headers">
-                    <h1>
-                        BACK END
-                    </h1>
-                </div>
-                <div className="uml-diagram">
-                    <img className="sft-grid-box" src={UMLDiagramEzQuiz}/>
-                </div>
-            </div>
         </div>
     </>
+}
+
+function expandClicked() {
+    var collapse: HTMLInputElement = (document.querySelector("#collapse") as HTMLInputElement);
+    var btn: any = document.querySelector(".sft-engin .expand-button");
+    btn.textContent = collapse.checked ? "CLICK TO EXPAND!" : "CLICK TO COLLAPSE";
+    collapse.checked = !collapse.checked;
 }
