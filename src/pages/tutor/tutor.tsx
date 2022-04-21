83d1bba003b79a80cfa98eeff48f4e34e1e1e7e1
Ben Schreiber <bpschreiber2003@gmail.com>
2022-04-21 00:37:01 -0700

    demo contact done

--- src/pages/tutor/tutor.tsx
+++ src/pages/tutor/tutor.tsx
@@ -38,10 +38,26 @@ export default function TutorPage() {
                 </div>
             </div>
 
-            <div>
-                <a href="/contact" className="contact">
-                    CONTACT ME
-                </a>
+            <div className="contact">
+                <div className="contact-form-group">
+                    <label htmlFor="Name" className="contact-label">Name</label>
+                    <div className="contact-input">
+                        <input type="text" id="Name" name="Name" className="contact-form-control" required />
+                    </div>
+                </div>
+                <div className="contact-form-group">
+                    <label htmlFor="Email" className="contact-label">Email Address</label>
+                    <div className="contact-input">
+                        <input type="email" id="Email" name="Email" className="contact-form-control" required />
+                    </div>
+                </div>
+                <div className="contact-form-group">
+                    <label htmlFor="Message" className="contact-label">Message</label>
+                    <div className="contact-input">
+                        <textarea id="Message" name="Message" className="contact-form-control" rows={6} maxLength={3000} required></textarea>
+                    </div>
+                </div>
+                <button className="contact-send-button">SEND</button>
             </div>
 
         </div>
