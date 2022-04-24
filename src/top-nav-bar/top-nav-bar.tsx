cb1d15c6485d6e46d5c16f25b1cca4741a4e7975
bens-schreiber <twistedillusion1@gmail.com>
2022-04-24 11:23:32 -0700

    renovated top-nav-bar css

--- src/top-nav-bar/top-nav-bar.tsx
+++ src/top-nav-bar/top-nav-bar.tsx
@@ -1,26 +1,29 @@
 import Logo2 from '../assets/logo2.svg'
 export default function TopNavBar(): JSX.Element {
     return <>
-        <div className="top-navbar-container">
-            <a href="/" className="logo2-container">
-                <img src={Logo2} alt="" />
-            </a>
-            <label htmlFor="nav-menu-toggle" className="nav-icon">
-                &#9776;
-            </label>
-        </div>
-        <input type="checkbox" id="nav-menu-toggle" />
-        <div className="nav-menu nav-menu-slide-in">
-            <nav >
-                <h1><a href="/">HOME</a></h1>
-                <ul>
-                    <li><a href="/contact">CONTACT</a></li>
-                    <li><a href="/">ABOUT</a></li>
-                    <li><a href="/tutor">CS TUTORING</a></li>
-                    <li><a href="#">DISCORD BOTS</a></li>
-                    <li><a href="/portfolio">PORTFOLIO</a></li>
-                </ul>
-            </nav>
+        <div className="tnb">
+
+            <div className="tnb-container">
+                <img id="tnb-logo" className="tnb-clickable" src={Logo2} alt="" />
+                <label htmlFor="tnb-dropdown-toggle" className="tnb-dropdown-toggle-icon tnb-clickable">
+                    &#9776;
+                </label>
+            </div>
+
+            <input type="checkbox" id="tnb-dropdown-toggle" />
+            <div className="tnb-nav tnb-animate-dropdown">
+                <nav className="tnb-links" >
+                    <h1><a href="/">HOME</a></h1>
+                    <ul className="tnb-tree-stem">
+                        <li className="tnb-branch"><a href="/contact">CONTACT</a></li>
+                        <li className="tnb-branch"><a href="/">ABOUT</a></li>
+                        <li className="tnb-branch"><a href="/tutor">CS TUTORING</a></li>
+                        <li className="tnb-branch"><a href="#">DISCORD BOTS</a></li>
+                        <li className="tnb-branch"><a href="/portfolio">PORTFOLIO</a></li>
+                    </ul>
+                </nav>
+            </div>
+
         </div>
     </>
 }
