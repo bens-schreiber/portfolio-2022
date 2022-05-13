74d3d2bb000fb872e36c93a60c778ea54f6ed610
bens-schreiber <twistedillusion1@gmail.com>
2022-05-12 17:01:13 -0700

    removed bind for collapse function, made it into arrow function

--- src/pages/home/components/quicklinks/quicklinks.tsx
+++ src/pages/home/components/quicklinks/quicklinks.tsx
@@ -20,9 +20,6 @@ export default class Quicklinks
         super(props);
         this.props = props;
 
-        //Bind the collapse method to this component
-        this.collapse = this.collapse.bind(this);
-
         this.state = {
             collapsed: true
         }
@@ -31,24 +28,23 @@ export default class Quicklinks
     private QuickLinkExpanded = () =>
         <>
             <ul className="ql-expanded-container ql-links ql-tree-stem">
-                <button className="ql-expanded-title clickable" onClick={this.collapse}>{this.props.title}</button>
+                <button className="ql-expanded-title clickable" 
+                onClick={() => this.setState({ collapsed: !this.state.collapsed })}>{this.props.title}</button>
                 {this.props.children}
             </ul>
         </>
 
-    public static Item = (props: {link: string, name: string}) =>
+    public static Item = (props: { link: string, name: string }) =>
         <>
             <li className="ql-item ql-tree-branch"><a href={props.link}>{props.name}</a></li>
         </>
-
-    collapse = () => this.setState({collapsed: !this.state.collapsed})
 
     render = () =>
         <>
             <div className="ql-container">
 
                 <When condition={this.state.collapsed}>
-                    <QLButton title={this.props.title} action={this.collapse} />
+                    <QLButton title={this.props.title} action={() => this.setState({ collapsed: !this.state.collapsed })} />
                 </When>
 
                 <When condition={!this.state.collapsed}>
