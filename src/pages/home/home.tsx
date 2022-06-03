10d75fe113139548078c56f202f9a61aabf9d05b
bens-schreiber <twistedillusion1@gmail.com>
2022-06-03 13:31:11 -0700

    css reworks

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,31 +3,29 @@ import Quicklinks, { QLItem } from "./components/quicklinks/quicklinks";
 import { Logo1 } from "../../assets";
 export default function Home(): JSX.Element {
     return <>
-        <div className="home-page">
-            <div className="hp-front-container">
-                <div className="hp-front hp-content-width">
+        <div className="hp-container">
+            <div className="hp-items hp-content-width">
 
+                <img id="hp-front-logo" src={Logo1} alt="" />
 
-                    <img id="hp-front-logo" src={Logo1} alt="" />
-                    
-                    <div className="hp-dropdown-btns">
+                <div className="hp-dropdown-btns">
 
-                        <Quicklinks title="SERVICES">
-                            <QLItem link="/tutor" name="CS TUTORING" />
-                            <QLItem link="/tutor" name="SCHEDULE TUTORING" />
-                        </Quicklinks>
+                    <Quicklinks title="SERVICES">
+                        <QLItem link="/tutor" name="CS TUTORING" />
+                        <QLItem link="/tutor" name="SCHEDULE TUTORING" />
+                    </Quicklinks>
 
-                        <Quicklinks title="PORTFOLIO">
-                            <QLItem link="/portfolio" name="SOFTWARE ENGINEER" />
-                            <QLItem link="/portfolio" name="MOBILE DEVELOPMENT" />
-                            <QLItem link="/portfolio" name="WEBSITE DESIGN" />
-                            <QLItem link="/portfolio" name="GAME DESIGN" />
-                        </Quicklinks>
+                    <Quicklinks title="PORTFOLIO">
+                        <QLItem link="/portfolio" name="SOFTWARE ENGINEER" />
+                        <QLItem link="/portfolio" name="MOBILE DEVELOPMENT" />
+                        <QLItem link="/portfolio" name="WEBSITE DESIGN" />
+                        <QLItem link="/portfolio" name="GAME DESIGN" />
+                    </Quicklinks>
 
-                    </div>
-
-                    <Contact />
                 </div>
+
+                <Contact />
+
             </div>
         </div>
     </>
