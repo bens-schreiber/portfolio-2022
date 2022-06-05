772fac64f343aed364dd47c04dba11275a0cbbcf
bens-schreiber <twistedillusion1@gmail.com>
2022-06-04 18:46:24 -0700

    changed svgs

--- src/pages/resume/resume.tsx
+++ src/pages/resume/resume.tsx
@@ -1,16 +1,18 @@
 import CareerSVG from "./assets/careers"
 import { Skills, FaSkillsItem, SkillsItem } from "./components/skills"
-import { faGolang, faJava, faJsSquare } from "@fortawesome/free-brands-svg-icons"
 import CppLogo from "./assets/cpp.svg"
 import DartLogo from "./assets/dart.svg"
+import JavaLogo from "./assets/java.svg"
+import GolangLogo from "./assets/golang.svg"
+import JsLogo from "./assets/js.svg"
 export const Resume = () => <>
     <div className="resume-container">
         <CareerSVG />
         <Skills>
-            <FaSkillsItem img={faJsSquare} header="JavaScript" desc="React, AnimeJS"/>
-            <FaSkillsItem img={faGolang} header="Go" desc="Gin, SQL"/>
-            <FaSkillsItem img={faJava} header="Java" desc="Springboot, JavaFX, Gradle" />
+            <SkillsItem img={JsLogo} header="JavaScript" desc="React, AnimeJS"/>
             <SkillsItem img={CppLogo} header="C++" desc="Qt Framework" />
+            <SkillsItem img={JavaLogo} header="Java" desc="Springboot, JavaFX, Gradle" />
+            <SkillsItem img={GolangLogo} header="Go" desc="Gin, SQL"/>
             <SkillsItem img={DartLogo} header="Dart" desc="Flutter Framework"/>
         </Skills>
     </div>
