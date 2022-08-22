adf3ad8206d5e26666e2f2301628d2ebbbf09230
benjamin schreiber <bpschreiber2003@gmail.com>
2022-08-22 16:02:55 -0700

    fixed unused var

--- src/pages/contact/contact-page.tsx
+++ src/pages/contact/contact-page.tsx
@@ -1,4 +1,4 @@
-import { Contact, UnanimatedContact } from "./components/contact"
+import { UnanimatedContact } from "./components/contact"
 import emailjs from 'emailjs-com';
 const ContactPage = (): JSX.Element =>
     <>
